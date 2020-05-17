0c4cb024863af3fb854162ab3a4ee361450d23e6
shivendrasingh <wb-shivendra619048@antfin.com>
2020-05-17 15:20:00 +0530

    init

---
+++ src/main/java/com/aritsd/arit/AritApplication.java
@@ -0,0 +1,13 @@
+package com.aritsd.arit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AritApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aritsd/arit/AritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aritsd.arit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
