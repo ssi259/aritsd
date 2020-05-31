4b0e0e4c04097b96be1e76477780f403e0eaa2f1
Prashant.Kumar <prashant.kumar@gaana.com>
2020-05-31 10:42:20 +0530

    c

--- src/main/java/service/DoctorServiceImpl.java
+++ src/main/java/service/DoctorServiceImpl.java
@@ -1,10 +1,29 @@
 package service;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import repository.ArDoctorRepository;
+
 /**
  * @author Prashant
  */
 public class DoctorServiceImpl implements DoctorService {
+ private Logger logger = LoggerFactory.getLogger(DoctorServiceImpl.class);
+ @Autowired
+ private ArDoctorRepository arDoctorRepository;
 
+ public ResponseEntity<?> createDoctorProfile(){
+  try {
+   //
 
-    
+  }catch (Exception e){
+   logger.error("Error while creating Doctor profile",e);
+   return ResponseEntity.ok(0);
+  }
+   ResponseEntity.ok(1);
+  return null;
+ }
+
 }
